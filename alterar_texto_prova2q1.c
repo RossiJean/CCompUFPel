16affcc8d7ddf800230c038e74ce219f61adbd40
Jean Rossi <rossisljean@gmail.com>
2026-08-01 05:22:48 -0300

    Refactor formatting and indentation for improved readability in alterar_texto_prova2q1.c

--- alterar_texto_prova2q1.c
+++ alterar_texto_prova2q1.c
@@ -2,62 +2,44 @@
 #include <string.h>
 
 char vogais[] = {
-    'a',
-    'e',
-    'i',
-    'o',
-    'u',
-    'A',
-    'E',
-    'I',
-    'O',
-    'U',
+    'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U',
 };
 int tamanho_vogais = sizeof(vogais) / sizeof(vogais[0]);
 
 void mascarar_texto(char texto[]);
 
-int main()
-{
+int main() {
 
-    char texto[100];
+  char texto[100];
 
-    printf("Digite seu texto: ");
-    fgets(texto, sizeof(texto), stdin);
+  printf("Digite seu texto: ");
+  fgets(texto, sizeof(texto), stdin);
 
-    mascarar_texto(texto);
+  mascarar_texto(texto);
 
-    printf("\n%s", texto);
+  printf("\n%s", texto);
 
-    return 0;
+  return 0;
 }
 
-void mascarar_texto(char texto[])
-{
-    int tamanho = strlen(texto);
-    for (int i = 0; i < tamanho; i++)
-    {
-        int eh_vogal = 0;
-        for (int j = 0; j < tamanho_vogais; j++)
-        {
-            if (texto[i] == vogais[j])
-            {
-                texto[i] = '#';
-                eh_vogal = 1;
-                break;
-            }
-        }
-
-        if (eh_vogal == 0)
-        {
-            if (texto[i] >= 'a' && texto[i] <= 'z')
-            {
-                texto[i] = texto[i] - 32;
-            }
-            else if (texto[i] >= 'A' && texto[i] <= 'Z')
-            {
-                texto[i] = texto[i] + 32;
-            }
-        }
+void mascarar_texto(char texto[]) {
+  int tamanho = strlen(texto);
+  for (int i = 0; i < tamanho; i++) {
+    int eh_vogal = 0;
+    for (int j = 0; j < tamanho_vogais; j++) {
+      if (texto[i] == vogais[j]) {
+        texto[i] = '#';
+        eh_vogal = 1;
+        break;
+      }
     }
+
+    if (eh_vogal == 0) {
+      if (texto[i] >= 'a' && texto[i] <= 'z') {
+        texto[i] = texto[i] - 32;
+      } else if (texto[i] >= 'A' && texto[i] <= 'Z') {
+        texto[i] = texto[i] + 32;
+      }
+    }
+  }
 }
